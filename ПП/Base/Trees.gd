4ff26c0f14915294aff12b664902fa761b2682b4
YanDanilin <92442472+YanDanilin@users.noreply.github.com>
2022-03-20 12:06:22 +0300

    Add files via upload

---
+++ ПП/Base/Trees.gd
@@ -0,0 +1,20 @@
+extends TileMap
+
+var VERT = 52
+var HOR = 52
+
+func _ready():
+	clear()
+	randomize()
+	_gen_trees()
+	pass 
+
+func _gen_trees():
+	var filling :int = 0.095 * VERT * HOR
+	var coord_x
+	var coord_y
+	for i in range(filling):
+		coord_x = randi()%HOR
+		coord_y = randi()%VERT
+		if get_cell(coord_x, coord_y) == INVALID_CELL:
+			set_cell(coord_x - HOR/2, coord_y - VERT/2, randi()%1)
