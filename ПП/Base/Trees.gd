86a6230a9fe737c1e630d6e61b1bb6c5b415f912
YanDanilin <92442472+YanDanilin@users.noreply.github.com>
2022-04-24 16:28:54 +0300

    Add files via upload

--- ПП/Base/Trees.gd
+++ ПП/Base/Trees.gd
@@ -9,12 +9,14 @@ func _ready():
 	_gen_trees()
 	pass 
 
+
+
 func _gen_trees():
-	var filling :int = 0.05 * VERT * HOR
+	var filling :int = 0.03 * VERT * HOR
 	var coord_x
 	var coord_y
 	for i in range(filling):
 		coord_x = randi()%HOR
 		coord_y = randi()%VERT
-		if get_cell(coord_x, coord_y) == INVALID_CELL:
+		if get_cell(coord_x, coord_y) == INVALID_CELL: 
 			set_cell(coord_x - HOR/2, coord_y - VERT/2, randi()%2)
